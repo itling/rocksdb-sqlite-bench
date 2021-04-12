3ec4680a3b89caf0580f6f0a6cfe17879bb6a243
itling <3168582@qq.com>
2021-04-12 15:20:54 +0800

    init commit

--- benches/rocksdb.rs
+++ benches/rocksdb.rs
@@ -1,78 +1,97 @@
+use cid::{Cid, Codec};
 use criterion::{criterion_group, criterion_main, Criterion};
-use rocksdb::{DB, Writable};
-use cid::{Cid,Codec};
 use multihash::Sha2_256;
+use rocksdb::{Writable, DB};
 
 fn monotonic_crud(c: &mut Criterion) {
     let mut group = c.benchmark_group("rocksdb_monotonic_crud");
 
-    let db = DB::open_default("/data/rocksdb_rs_crud").unwrap();
+    let db = DB::open_default("rocksdb_rs_crud").unwrap();
 
     let mut bytes_count = 0_u32;
     let mut bytes = |len| -> Vec<u8> {
         bytes_count += 1;
-        bytes_count.to_be_bytes().iter().cycle().take(len).copied().collect()
+        bytes_count
+            .to_be_bytes()
+            .iter()
+            .cycle()
+            .take(len)
+            .copied()
+            .collect()
     };
 
-    let max_id=db.get(b"max_id").unwrap();
-    let mut init_count=0_u32;
-    if let Some(id)= max_id{
+    let max_id = db.get(b"max_id").unwrap();
+    let mut init_count = 0_u32;
+    if let Some(id) = max_id {
         let mut init_array: [u8; 4] = Default::default();
         init_array.copy_from_slice(&*id);
-        init_count= u32::from_be_bytes(init_array)+1;
+        init_count = u32::from_be_bytes(init_array);
         println!("get max id num={:?}", init_count);
-   }
-  
- 
-    if init_count<2000000{ 
-        for _i in 0..2000000{ 
-            let cid = Cid::new_v1(Codec::Raw, Sha2_256::digest(&init_count.to_be_bytes().to_vec()));
-            db.put(&cid.to_bytes().to_vec(), &bytes(1024*1024)).unwrap();
-            init_count+=1;
-            if init_count%1000==0{
+    }
+
+    //调整循环次数,1条数据等于1m
+    if init_count < 100000 {
+        for _i in 0..100000 {
+            let cid = Cid::new_v1(
+                Codec::Raw,
+                Sha2_256::digest(&init_count.to_be_bytes().to_vec()),
+            );
+            db.put(&cid.to_bytes().to_vec(), &bytes(1024 * 1024))
+                .unwrap();
+            init_count += 1;
+            if init_count % 1000 == 0 {
                 println!("init  record {:?}", init_count);
             }
         }
+        println!("insert max id={:?}", init_count);
+        let _ = db
+            .put(b"max_id", &init_count.to_be_bytes().to_vec())
+            .unwrap();
     }
 
-    //data size=1m
     let mut insert_count = init_count;
     group.bench_function("monotonic inserts", |b| {
         b.iter(|| {
+            let cid = Cid::new_v1(
+                Codec::Raw,
+                Sha2_256::digest(&insert_count.to_be_bytes().to_vec()),
+            );
+            db.put(&cid.to_bytes().to_vec(), &bytes(1024 * 1024))
+                .unwrap();
             insert_count += 1;
-            let cid = Cid::new_v1(Codec::Raw, Sha2_256::digest(&insert_count.to_be_bytes().to_vec()));
-            db.put(&cid.to_bytes().to_vec(), &bytes(1024*1024)).unwrap();
         })
     });
     println!("insert max id={:?}", insert_count);
-    let _=db.put(b"max_id",&insert_count.to_be_bytes().to_vec()).unwrap();
+    let _ = db
+        .put(b"max_id", &insert_count.to_be_bytes().to_vec())
+        .unwrap();
 
-    let mut get_count=insert_count;
+    let mut get_count = insert_count;
     group.bench_function("monotonic gets", |b| {
         b.iter(|| {
             get_count -= 1;
-            let cid = Cid::new_v1(Codec::Raw, Sha2_256::digest(&get_count.to_be_bytes().to_vec()));
+            let cid = Cid::new_v1(
+                Codec::Raw,
+                Sha2_256::digest(&get_count.to_be_bytes().to_vec()),
+            );
             db.get(&cid.to_bytes().to_vec()).unwrap();
         })
     });
 
-    let mut remove_count=insert_count;
+    let mut remove_count = insert_count;
     println!("removals max id={:?}", remove_count);
     group.bench_function("monotonic removals", |b| {
         b.iter(|| {
             remove_count -= 1;
-            let cid = Cid::new_v1(Codec::Raw, Sha2_256::digest(&remove_count.to_be_bytes().to_vec()));
+            let cid = Cid::new_v1(
+                Codec::Raw,
+                Sha2_256::digest(&remove_count.to_be_bytes().to_vec()),
+            );
             db.delete(&cid.to_bytes().to_vec()).unwrap();
         })
     });
-    println!("removals max id={:?}", remove_count);
-
     group.finish();
 }
 
-
-criterion_group!(
-    benches,
-    monotonic_crud,
-);
+criterion_group!(benches, monotonic_crud,);
 criterion_main!(benches);
--- benches/sqlite.rs
+++ benches/sqlite.rs
@@ -1,8 +1,7 @@
+use cid::{Cid, Codec};
 use criterion::{criterion_group, criterion_main, Criterion};
-use rusqlite::{params, Connection,NO_PARAMS};
-use cid::{Cid,Codec};
 use multihash::Sha2_256;
-
+use rusqlite::{params, Connection, NO_PARAMS};
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Record {
@@ -13,15 +12,13 @@ pub struct Record {
     pub value: Vec<u8>,
 }
 
-
-fn sqlite_bulk_load(c: &mut Criterion){
-
+fn sqlite_bulk_load(c: &mut Criterion) {
     let mut group = c.benchmark_group("sqlite_monotonic_crud");
-    
+
     // let temp_dir = tempfile::tempdir().unwrap();
     // let path = temp_dir.path().join("test_insert.db");
 
-    let mut conn = Connection::open("/data/sqlite_bulk_load.db").unwrap();
+    let mut conn = Connection::open("sqlite_bulk_load.db").unwrap();
 
     //create table
     conn.execute(
@@ -30,104 +27,123 @@ fn sqlite_bulk_load(c: &mut Criterion){
                   key             BLOB NOT NULL,
                   value           BLOB NOT NULL
                   )",
-                  NO_PARAMS        
-    ).unwrap();
+        NO_PARAMS,
+    )
+    .unwrap();
 
     // create index for id
     conn.execute(
         "CREATE index  IF NOT EXISTS index_record_id on record(id)",
-                  NO_PARAMS        
-    ).unwrap();
+        NO_PARAMS,
+    )
+    .unwrap();
 
-      // create index for key
-      conn.execute(
+    // create index for key
+    conn.execute(
         "CREATE index  IF NOT EXISTS index_record_key on record(key)",
-                  NO_PARAMS        
-    ).unwrap();
+        NO_PARAMS,
+    )
+    .unwrap();
 
-
-    let mut init_count=0;
-    let max_id_r=conn.query_row("SELECT max(id) FROM record", NO_PARAMS, |row| row.get(0));
-    if max_id_r.is_ok(){
-        let max_id:u32=max_id_r.unwrap();
-        init_count=max_id+1;
+    let mut init_count = 0;
+    let max_id_r = conn.query_row("SELECT max(id) FROM record", NO_PARAMS, |row| row.get(0));
+    if max_id_r.is_ok() {
+        let max_id: u32 = max_id_r.unwrap();
+        init_count = max_id + 1;
     }
-    println!("init_count id={}",init_count);
-   
+    println!("init_count id={}", init_count);
+
     let mut bytes_count = 0_u32;
     let mut bytes = |len| -> Vec<u8> {
         bytes_count += 1;
-        bytes_count.to_be_bytes().iter().cycle().take(len).copied().collect()
+        bytes_count
+            .to_be_bytes()
+            .iter()
+            .cycle()
+            .take(len)
+            .copied()
+            .collect()
     };
 
-    //init data 100g
-    if init_count<100000{
-        for  _ in 0..100 {
+    //调整循环次数,1条数据等于1m 1000条提交一次
+    if init_count < 100000 {
+        for _ in 0..100 {
             let tx = conn.transaction().unwrap();
-            let cid = Cid::new_v1(Codec::Raw, Sha2_256::digest(&init_count.to_be_bytes().to_vec()));
-            for  _ in 0..1000 {
+            let cid = Cid::new_v1(
+                Codec::Raw,
+                Sha2_256::digest(&init_count.to_be_bytes().to_vec()),
+            );
+            for _ in 0..1000 {
                 let record = Record {
-                    id:init_count,
+                    id: init_count,
                     key: cid.to_bytes().to_vec(),
-                    value: bytes(1024*1024),
+                    value: bytes(1024 * 1024),
                 };
                 tx.execute(
                     "INSERT INTO record (id,key, value) VALUES (?1, ?2,?3)",
-                    params![record.id,record.key, record.value],
-                ).unwrap();
-                init_count=init_count+1;
+                    params![record.id, record.key, record.value],
+                )
+                .unwrap();
+                init_count = init_count + 1;
             }
-            if init_count%100==0{
+            if init_count % 100 == 0 {
                 println!("init  record {:?}", init_count);
             }
-            let _=tx.commit();
+            let _ = tx.commit();
         }
     }
 
-    let mut insert_count=init_count;
-    let mut insert_ids:Vec<u32>=vec![];
+    let mut insert_count = init_count;
+    let mut insert_ids: Vec<u32> = vec![];
     group.bench_function("monotonic insert", |b| {
         b.iter(|| {
-                let cid = Cid::new_v1(Codec::Raw, Sha2_256::digest(&insert_count.to_be_bytes().to_vec()));
-                let record = Record {
-                    id:insert_count,
-                    key: cid.to_bytes().to_vec(),
-                    value:bytes(1024*1024),
-                };
-                conn.execute(
-                    "INSERT INTO record (id,key, value) VALUES (?1, ?2,?3)",
-                    params![record.id,record.key, record.value],
-                ).unwrap();
-                insert_ids.push(insert_count);
-              
-                insert_count=insert_count+1;
+            let cid = Cid::new_v1(
+                Codec::Raw,
+                Sha2_256::digest(&insert_count.to_be_bytes().to_vec()),
+            );
+            let record = Record {
+                id: insert_count,
+                key: cid.to_bytes().to_vec(),
+                value: bytes(1024 * 1024),
+            };
+            conn.execute(
+                "INSERT INTO record (id,key, value) VALUES (?1, ?2,?3)",
+                params![record.id, record.key, record.value],
+            )
+            .unwrap();
+            insert_ids.push(insert_count);
+
+            insert_count = insert_count + 1;
         })
     });
     println!("insert_ids len ={:?}", insert_ids.len());
 
-    let mut get_index=0;
+    let mut get_index = 0;
     group.bench_function("monotonic query", |b| {
         b.iter(|| {
-            if insert_ids.len()-1==get_index{
+            if insert_ids.len() - 1 == get_index {
                 return;
             }
-            let get_count=insert_ids.get(get_index).unwrap();
-            let cid = Cid::new_v1(Codec::Raw, Sha2_256::digest(&get_count.to_be_bytes().to_vec()));
-            let query_r:Vec<u8>=conn.query_row("SELECT * FROM record where key = ?1 ",  params![cid.to_bytes().to_vec()], |row| row.get(2)).unwrap();
-            assert_eq!(query_r.len()>0,true); 
-            get_index+=1;
+            let get_count = insert_ids.get(get_index).unwrap();
+            let cid = Cid::new_v1(
+                Codec::Raw,
+                Sha2_256::digest(&get_count.to_be_bytes().to_vec()),
+            );
+            let query_r: Vec<u8> = conn
+                .query_row(
+                    "SELECT * FROM record where key = ?1 ",
+                    params![cid.to_bytes().to_vec()],
+                    |row| row.get(2),
+                )
+                .unwrap();
+            assert_eq!(query_r.len() > 0, true);
+            get_index += 1;
         })
     });
-
 
     group.finish();
 }
 
-
-
-criterion_group!(
-    benches,
-    sqlite_bulk_load,
-);
+criterion_group!(benches, sqlite_bulk_load,);
 
 criterion_main!(benches);
